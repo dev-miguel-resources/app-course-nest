14ded8abf2dc16dee995b85205274bddb311ae58
dev-miguel-resources <ingenieromiguelch@gmail.com>
2024-02-24 13:32:32 -0300

    providers type

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 
-@Controller()
-export class StudentController {}
+import { StudentService } from './student.service';
+
+@Controller('students') // app.use("/students", StudentController), así sería en express
+export class StudentController {
+  constructor(
+    @Inject('STUDENT') private readonly studentService: StudentService,
+    @Inject('CONFIG') private readonly config: { url: string },
+    @Inject('CONFIG2') private readonly config2: { url: string },
+    @Inject('CONFIG3')
+    private readonly config3: { url: string; urlConfig: string },
+  ) {}
+
+  @Get('/')
+  async getAll() {
+    const students = await this.studentService.getAll();
+
+    // esto devuelve lo del enroll + studentService
+    return [
+      ...students,
+      this.config.url,
+      this.config2.url,
+      this.config3.url,
+      this.config3.urlConfig,
+    ];
+  }
+}
