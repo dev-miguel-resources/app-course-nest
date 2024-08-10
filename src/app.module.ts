50611d5a7916fd8f073dc1812dbe3e62ec54d702
dev-miguel-resources <ingenieromiguelch@gmail.com>
2024-08-10 14:42:12 -0400

    implementing cqrs pattern

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
 import { UserModule } from './modules/user/infraestructure/presentation/user.module';
 import { RoleModule } from './modules/role/interfaces/role.module';
+import { AppoinmentModule } from './modules/appoinment/infraestructure/presentation/appointment.module';
 
 @Module({
-  imports: [StudentModule, UserModule, RoleModule],
+  imports: [StudentModule, UserModule, RoleModule, AppoinmentModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/appoinment/infraestructure/presentation/appoinment.controller.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { AppoinmentCLCommand } from '../../application/commands/appointment.cl.command';
+import { AppoinmentCOCommand } from '../../application/commands/appointment.co.command';
+import { AppoinmentMXCommand } from '../../application/commands/appointment.mx.command';
+import { CommandBus } from '@nestjs/cqrs';
+import { AppoinmentCreateDTO } from './dtos/appoinment.create.dto';
+
+const countryCommands = {
+  CL: AppoinmentCLCommand,
+  CO: AppoinmentCOCommand,
+  MX: AppoinmentMXCommand,
+};
+
+@Controller('appointments')
+export class AppointmentController {
+  // recibes una dy
+  constructor(private readonly commandBus: CommandBus) {}
+
+  @Post()
+  async createAppointment(@Body() appointment: AppoinmentCreateDTO) {
+    // recibir el código del país para saber a que lógica (handler) debe derivarlo
+    const appointmentCommand = new countryCommands[appointment.countryIso]();
+    // armamos un objeto con el código del país y los datos
+    Object.assign(appointmentCommand, appointment);
+
+    // el command bus una vez tiene preparado los datos deriva la ejecución del comando al handler respectivo de acuerdo al código recibido
+    const idReturned = await this.commandBus.execute(appointmentCommand);
+
+    return { idReturned };
+  }
+}
